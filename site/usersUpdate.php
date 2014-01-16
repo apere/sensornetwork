c450583ee3eab57d352db85e700916c3528a7c49
Adam Pere <adam@goinvo.com>
2014-01-16 17:51:11 -0500

    added code to (hopefully) add data to the mysql db (untested)

--- site/usersUpdate.php
+++ site/usersUpdate.php
@@ -1,13 +1,34 @@
 <?php
- 
-// load in mysql server configuration (connection string, user/pw, etc)
-include 'mysqlConfig.php';
-// connect to the database
-@mysql_select_db($dsn) or die( "Unable to select database");
- 
-// updates the map db
- 
-$query="UPDATE `v_map` SET `height`=".$_GET["height"]." WHERE `col`= ".$_GET["col"]." and `row`= ".$_GET["row"]; //change this line with my DB info
-mysql_query($query);
-mysql_close();
-?>
+    $dbhost = 'us-cdbr-east-04.cleardb.com';
+    $dbuser = 'b01556d5fadf7a';
+    $dbpass = '4ac9b1e3';
+    $conn = mysql_connect($dbhost, $dbuser, $dbpass);
+    
+    if(! $conn )
+    {
+      die('Could not connect: ' . mysql_error());
+    }
+
+    //If statement to determine whether or not we want to add
+    //a new user or position
+    if(array_key_exists("user", $_GET)) {
+        $sql = 'INSERT
+            INTO users (ID, user, color, tracking)
+            VALUES ({$_GET["ID"]}, {$_GET["user"]}, {$_GET["color"]}, 1)';
+    }
+    else if(array_key_exists("x", $_GET){
+       $sql = 'INSERT
+            INTO positions (ID, x, y, z)
+            VALUES ({$_GET["ID"]}, {$_GET["x"]}, {$_GET["y"]}, {$_GET["z"]})'; 
+    }
+     
+    
+    
+    mysql_select_db('heroku_357d3113aeb360f');
+    $retval = mysql_query( $sql, $conn );
+
+    
+    mysql_close($conn);
+    ?>
+
+
